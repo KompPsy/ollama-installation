fb624e030cc0d6ab22be430f3fb9cf5ffe9a4683
Peter Aldrich Jr <36713891+KompPsy@users.noreply.github.com>
2025-04-27 00:53:50 -0400

    Update automate-ollama-install.sh

--- automate-ollama-install.sh
+++ automate-ollama-install.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+curl -sSL https://raw.githubusercontent.com/KompPsy/ollama-installation/main/install-docker.sh | sudo bash
 curl -sSL https://raw.githubusercontent.com/KompPsy/ollama-installation/main/stop-ollama.sh | sudo bash
 curl -sSL https://raw.githubusercontent.com/KompPsy/ollama-installation/main/install-ollama.sh | sudo bash
 curl -sSL https://raw.githubusercontent.com/KompPsy/ollama-installation/main/remove-webui-docker.sh | sudo bash
